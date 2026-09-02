39ae51aac8493b3ef2f7d451b8d2903f58ba721c
Guillaume Thouvenin <guillaume.thouvenin@vates.tech>
2026-09-02 20:37:55 +0200

    start reading JSON file

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Generates bindings for Zig following the approach introduced in https://github.com/contificate/xapy/.
+
+- build: `zig build`
+- run: `./zig-out/bin/zapi xapi.json`
--- src/main.zig
+++ src/main.zig
@@ -21,6 +21,19 @@ const AstType = union(AstTypeTag) {
     },
 };
 
-pub fn main() void {
-    std.debug.print("hello world!\n", .{});
+pub fn main(init: std.process.Init) !void {
+    var args = init.minimal.args.iterate();
+
+    // First args is the name of the binary, skip it
+    _ = args.next();
+
+    const fname = if (args.next()) |arg| arg else return error.NameIsMissing;
+
+    // read the file
+    var buffer: [4096]u8 = undefined;
+    const contents = try std.Io.Dir.readFile(std.Io.Dir.cwd(), init.io, fname, &buffer);
+    var tok = std.mem.tokenizeSequence(u8, contents, "\n");
+    while (tok.next()) |line| {
+        std.debug.print("line: {s}", .{line});
+    }
 }
