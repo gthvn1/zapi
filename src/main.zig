93285ee31d6f9520ecaa22b739340a3c96690e19
Guillaume Thouvenin <guillaume.thouvenin@vates.tech>
2026-09-02 16:52:42 +0200

    add AST types

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,26 @@
 const std = @import("std");
 
+const AstTypeTag = enum { builtin, ref, set, option, class, @"enum", map, record };
+
+const AstType = union(AstTypeTag) {
+    builtin: []const u8,
+    ref: *AstType,
+    set: *AstType,
+    option: *AstType,
+    class: []const u8,
+    @"enum": []const u8,
+    map: struct {
+        key_type: *AstType,
+        value_type: *AstType,
+    },
+    record: []struct {
+        name: []const u8,
+        type: *AstType,
+        description: []const u8,
+        required: bool,
+    },
+};
+
 pub fn main() void {
     std.debug.print("hello world!\n", .{});
 }
