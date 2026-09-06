39a1aea8e677446622eda62bb2edf32c3f1b36b3
Guillaume <gthvn1@gmail.com>
2026-09-06 18:09:23 +0200

    split code and remove dead code

--- src/main.zig
+++ src/main.zig
@@ -1,26 +1,7 @@
 const std = @import("std");
 const json = std.json;
 
-const AstTypeTag = enum { builtin, ref, set, option, class, @"enum", map, record };
-
-const AstType = union(AstTypeTag) {
-    builtin: []const u8,
-    ref: *AstType,
-    set: *AstType,
-    option: *AstType,
-    class: []const u8,
-    @"enum": []const u8,
-    map: struct {
-        key_type: *AstType,
-        value_type: *AstType,
-    },
-    record: []struct {
-        name: []const u8,
-        type: *AstType,
-        description: []const u8,
-        required: bool,
-    },
-};
+const ast = @import("xapi_ast.zig");
 
 pub fn main(init: std.process.Init) !void {
     var args = init.minimal.args.iterate();
@@ -43,75 +24,5 @@ pub fn main(init: std.process.Init) !void {
     const parsed = try json.parseFromSlice(std.json.Value, init.gpa, contents, .{});
     defer parsed.deinit();
 
-    var id: usize = 1;
-    // The top level JSON Value is one array.
-    // The array contains object that are objectMap.
-    for (parsed.value.array.items) |item| {
-        const object: std.json.ObjectMap = item.object;
-        if (object.get("name")) |name| {
-            std.debug.print("ClassInfo:\n{{\n  id: {d}, name: {s}\n", .{ id, name.string });
-        } else return error.nameIsMissing;
-        // all object should have a field that is an array
-        if (object.get("fields")) |fields_array| {
-            try parse_fields(fields_array);
-        } else return error.fieldsIsMissing;
-        // all object should have messages that is an array
-        if (object.get("messages")) |messages_array| {
-            try parse_messages(messages_array);
-        } else return error.messagesIsMissing;
-        std.debug.print("}}\n", .{});
-        id += 1;
-    }
-}
-
-// We are expecting an array
-fn parse_fields(fields: std.json.Value) !void {
-    std.debug.print("  fields:\n", .{});
-    for (fields.array.items) |item| {
-        const object: std.json.ObjectMap = item.object;
-        if (object.get("name")) |name| {
-            std.debug.print("    ({s}", .{name.string});
-        } else return error.fieldNameIsMissing;
-        if (object.get("type")) |ty| {
-            std.debug.print(", {s})\n", .{ty.string});
-        } else return error.fieldTypeIsMissing;
-    }
-}
-
-fn parse_messages(messages: std.json.Value) !void {
-    std.debug.print("  messages:\n", .{});
-    for (messages.array.items) |item| {
-        const object: std.json.ObjectMap = item.object;
-        if (object.get("name")) |name| {
-            std.debug.print("    ({s}", .{name.string});
-        } else return error.msgNameIsMissing;
-        if (object.get("params")) |p| {
-            try parse_msg_params(p);
-        }
-        if (object.get("result")) |r| {
-            try parse_msg_result(r);
-        }
-    }
-}
-
-fn parse_msg_params(params: std.json.Value) !void {
-    for (params.array.items) |item| {
-        const object: std.json.ObjectMap = item.object;
-        if (object.get("name")) |name| {
-            std.debug.print(", {s}:", .{name.string});
-        } else return error.paramNameIsMissing;
-        if (object.get("type")) |ty| {
-            std.debug.print("{s}", .{ty.string});
-        } else return error.paramTypeIsMissing;
-    }
-}
-
-fn parse_msg_result(result: std.json.Value) !void {
-    switch (result) {
-        .array => {
-            const res = result.array.items[0];
-            std.debug.print(", {s})\n", .{res.string});
-        },
-        else => return error.resultIsNotAnArray,
-    }
+    try ast.parse_api(parsed.value);
 }
---
+++ src/xapi_ast.zig
@@ -0,0 +1,85 @@
+const std = @import("std");
+const json = std.json;
+
+pub fn parse_api(root: std.json.Value) !void {
+    // Just to know the size of struct passed on the stack
+    std.debug.print("{d}\n", .{@sizeOf(std.json.Value)});
+
+    var id: usize = 1;
+    // The top level JSON Value is one array.
+    // The array contains object that are objectMap.
+    switch (root) {
+        .array => for (root.array.items) |item| {
+            const object: std.json.ObjectMap = item.object;
+            if (object.get("name")) |name| {
+                std.debug.print(
+                    "ClassInfo:\n{{\n  id: {d}, name: {s}\n",
+                    .{ id, name.string },
+                );
+            } else return error.nameIsMissing;
+            // all object should have a field that is an array
+            if (object.get("fields")) |fields_array| {
+                try parse_fields(fields_array);
+            } else return error.fieldsIsMissing;
+            // all object should have messages that is an array
+            if (object.get("messages")) |messages_array| {
+                try parse_messages(messages_array);
+            } else return error.messagesIsMissing;
+            std.debug.print("}}\n", .{});
+            id += 1;
+        },
+        else => return error.rootIsNotAnArray,
+    }
+}
+
+// We are expecting an array
+fn parse_fields(fields: std.json.Value) !void {
+    std.debug.print("  fields:\n", .{});
+    for (fields.array.items) |item| {
+        const object: std.json.ObjectMap = item.object;
+        if (object.get("name")) |name| {
+            std.debug.print("    ({s}", .{name.string});
+        } else return error.fieldNameIsMissing;
+        if (object.get("type")) |ty| {
+            std.debug.print(", {s})\n", .{ty.string});
+        } else return error.fieldTypeIsMissing;
+    }
+}
+
+fn parse_messages(messages: std.json.Value) !void {
+    std.debug.print("  messages:\n", .{});
+    for (messages.array.items) |item| {
+        const object: std.json.ObjectMap = item.object;
+        if (object.get("name")) |name| {
+            std.debug.print("    ({s}", .{name.string});
+        } else return error.msgNameIsMissing;
+        if (object.get("params")) |p| {
+            try parse_msg_params(p);
+        }
+        if (object.get("result")) |r| {
+            try parse_msg_result(r);
+        }
+    }
+}
+
+fn parse_msg_params(params: std.json.Value) !void {
+    for (params.array.items) |item| {
+        const object: std.json.ObjectMap = item.object;
+        if (object.get("name")) |name| {
+            std.debug.print(", {s}:", .{name.string});
+        } else return error.paramNameIsMissing;
+        if (object.get("type")) |ty| {
+            std.debug.print("{s}", .{ty.string});
+        } else return error.paramTypeIsMissing;
+    }
+}
+
+fn parse_msg_result(result: std.json.Value) !void {
+    switch (result) {
+        .array => {
+            const res = result.array.items[0];
+            std.debug.print(", {s})\n", .{res.string});
+        },
+        else => return error.resultIsNotAnArray,
+    }
+}
