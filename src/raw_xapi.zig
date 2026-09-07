075b7dc3b37fa04fd23dc0e4492d81552824d6c4
Guillaume Thouvenin <guillaume.thouvenin@vates.tech>
2026-09-07 11:28:14 +0200

    fill fields the right way

--- src/raw_xapi.zig
+++ src/raw_xapi.zig
@@ -3,8 +3,14 @@ const Self = @This();
 const std = @import("std");
 const json = std.json;
 
+pub const FieldInfo = struct {
+    name: []const u8,
+    type: []const u8,
+};
+
 pub const ClassInfo = struct {
     name: []const u8,
+    fields: []FieldInfo,
 };
 
 root: std.ArrayList(ClassInfo),
@@ -28,46 +34,52 @@ pub fn parse(self: *Self, root: std.json.Value) !void {
     std.debug.print("{d}\n", .{@sizeOf(std.json.Value)});
     const a = self.arena.allocator();
 
-    var id: usize = 1;
     // The top level JSON Value is one array.
     // The array contains object that are objectMap.
     switch (root) {
-        .array => for (root.array.items) |item| {
+        .array => for (root.array.items, 1..) |item, id| {
             const object: std.json.ObjectMap = item.object;
-            if (object.get("name")) |name| {
-                try self.root.append(a, ClassInfo{ .name = name.string });
-                std.debug.print(
-                    "ClassInfo:\n{{\n  id: {d}, name: {s}\n",
-                    .{ id, name.string },
-                );
-            } else return error.nameIsMissing;
+
+            const name = object.get("name") orelse return error.nameIsMissing;
+            std.debug.print("ClassInfo:\n{{\n  id: {d}, name: {s}\n", .{ id, name.string });
+
             // all object should have a field that is an array
-            if (object.get("fields")) |fields_array| {
-                try parse_fields(fields_array);
-            } else return error.fieldsIsMissing;
+            const fields = if (object.get("fields")) |fields_array|
+                try parse_fields(a, fields_array)
+            else
+                return error.fieldsIsMissing;
+
             // all object should have messages that is an array
             if (object.get("messages")) |messages_array| {
                 try parse_messages(messages_array);
             } else return error.messagesIsMissing;
             std.debug.print("}}\n", .{});
-            id += 1;
+
+            try self.root.append(a, .{
+                .name = name.string,
+                .fields = fields,
+            });
         },
         else => return error.rootIsNotAnArray,
     }
 }
 
-// We are expecting an array
-fn parse_fields(fields: std.json.Value) !void {
+fn parse_fields(a: std.mem.Allocator, fields: std.json.Value) ![]FieldInfo {
+    const f: []FieldInfo = try a.alloc(FieldInfo, fields.array.items.len);
+
     std.debug.print("  fields:\n", .{});
-    for (fields.array.items) |item| {
+    for (fields.array.items, f) |item, *slot| {
         const object: std.json.ObjectMap = item.object;
-        if (object.get("name")) |name| {
-            std.debug.print("    ({s}", .{name.string});
-        } else return error.fieldNameIsMissing;
-        if (object.get("type")) |ty| {
-            std.debug.print(", {s})\n", .{ty.string});
-        } else return error.fieldTypeIsMissing;
+        const name = object.get("name") orelse return error.fieldNameIsMissing;
+        const ty = object.get("type") orelse return error.fieldTypeIsMissing;
+
+        std.debug.print("    ({s}", .{name.string});
+        std.debug.print(", {s})\n", .{ty.string});
+
+        slot.* = .{ .name = name.string, .type = ty.string };
     }
+
+    return f;
 }
 
 fn parse_messages(messages: std.json.Value) !void {
