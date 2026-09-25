e4fe146775b8203ac6863189a1668d78538ce462
Guillaume Thouvenin <guillaume.thouvenin@vates.tech>
2026-09-25 13:53:18 +0200

    [tools] use stdout as output by default for debugging

--- tools/gen_readme.zig
+++ tools/gen_readme.zig
@@ -7,14 +7,19 @@ const std = @import("std");
 // have an uptodate README
 
 pub fn main(init: std.process.Init) !void {
+    // For debug purpose we can use stdout for printing
+    var out_buf: [1024]u8 = undefined;
+    var out_file: std.Io.File.Writer = .init(std.Io.File.stdout(), init.io, &out_buf);
+    const stdout = &out_file.interface;
+
     var args_it = init.minimal.args.iterate();
     // First parameter is the name of the program, skip it
     _ = args_it.next();
     // Now we are expecting the name of the file to read
     const fname = args_it.next() orelse return error.FileNameIsMissing;
-    std.debug.print("input: {s}\n", .{fname});
+    try stdout.print("input: {s}\n", .{fname});
 
-    // We need a reader for the file
+    // We need a file reader for fname
     const f = try std.Io.Dir.openFile(std.Io.Dir.cwd(), init.io, fname, .{});
     defer f.close(init.io);
 
@@ -22,7 +27,7 @@ pub fn main(init: std.process.Init) !void {
     var freader = f.reader(init.io, &fbuf);
 
     const flen = try f.length(init.io);
-    std.debug.print("Read {d} bytes\n", .{flen});
+    try stdout.print("Read {d} bytes\n", .{flen});
 
     const content = try freader.interface.readAlloc(init.gpa, @as(usize, flen));
     defer init.gpa.free(content);
@@ -30,9 +35,12 @@ pub fn main(init: std.process.Init) !void {
     const begin_idx = std.mem.find(u8, content, "BEGIN_CODE") orelse return;
     const end_idx = std.mem.find(u8, content, "END_CODE") orelse return error.EndCodeNotFound;
 
-    std.debug.print("Find Begin at offset {d}\n", .{begin_idx});
-    std.debug.print("Find End at offset {d}\n", .{end_idx});
+    try stdout.print("Find Begin at offset {d}\n", .{begin_idx});
+    try stdout.print("Find End at offset {d}\n", .{end_idx});
 
     // TODO: we have the offsets, now we probably want to have the begin line and end line to
     // get the name of the function that we need to insert here.
+
+    // Don't forget to flush
+    try stdout.flush();
 }
