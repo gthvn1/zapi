64eb9fc9638ba7ad9ea520762bfd1d47b6790284
Guillaume Thouvenin <guillaume.thouvenin@vates.tech>
2026-09-25 20:42:26 +0200

    [tools] almost done, still a bug to fix and build.zig to update

--- tools/gen_readme.zig
+++ tools/gen_readme.zig
@@ -6,6 +6,24 @@ const std = @import("std");
 // This file can be run when the code is build, and we will
 // have an uptodate README
 
+fn extractFileName(str: []u8) ?[]const u8 {
+    // We want to extrat filename under brackets
+    _, const after = std.mem.cut(u8, str, "[") orelse return null;
+    const fname, _ = std.mem.cut(u8, after, "]") orelse return null;
+    return std.mem.trim(u8, fname, " \n");
+}
+
+fn insertCode(w: *std.Io.Writer, io: std.Io, code: []const u8) !void {
+    // We need a file reader for fname
+    const f = try std.Io.Dir.openFile(std.Io.Dir.cwd(), io, code, .{});
+    defer f.close(io);
+
+    var fbuf: [1024]u8 = undefined;
+    var freader = f.reader(io, &fbuf);
+
+    _ = try freader.interface.streamRemaining(w);
+}
+
 pub fn main(init: std.process.Init) !void {
     // For debug purpose we can use stdout for printing
     var out_buf: [1024]u8 = undefined;
@@ -32,27 +50,34 @@ pub fn main(init: std.process.Init) !void {
     var content: std.Io.Writer.Allocating = .init(init.gpa);
     defer content.deinit();
 
-    var outside_block = true;
+    var code_block: ?[]const u8 = null;
 
     while (try fin.takeDelimiter('\n')) |line| {
         if (std.mem.find(u8, line, "<!-- BEGIN_CODE")) |_| {
-            // TODO: extract the name of the file
             try content.writer.print("{s}\n", .{line});
             try content.writer.writeAll("```zig\n");
-            outside_block = false;
-            // TODO: copy the content of the code
+            // TODO: Fix the bug. Currently code_block is a pointer to fbuf and a length. So
+            // right now it points to the filename of the code. But later we want to check that
+            // end_block is the same filename. But at this time the fbuf will be different and
+            // so code_block will be what is at the same address in fbuf but it won't be the
+            // filename. So we need to keep a copy here and free it once checked with END_CODE.
+            code_block = extractFileName(line) orelse return error.FailedToReadCodeFromBegin;
+            try insertCode(&content.writer, init.io, code_block.?);
             continue;
         }
 
         if (std.mem.find(u8, line, "<!-- END_CODE")) |_| {
             // TODO: check that name is matching the BEGIN CODE
+            // Sanity check
+            const code = extractFileName(line) orelse return error.FailedToReadCodeFromEnd;
+            if ((std.mem.eql(u8, code, code_block.?))) return error.MatchingFailedWithEndCode;
             try content.writer.writeAll("```\n");
             try content.writer.print("{s}\n", .{line});
-            outside_block = true;
+            code_block = null;
             continue;
         }
 
-        if (outside_block) {
+        if (code_block == null) {
             try content.writer.print("{s}\n", .{line});
         }
         // if inside block we have nothing to do since we already have the code
